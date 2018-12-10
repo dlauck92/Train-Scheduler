4403acd8185f509511cb4b24c57506fa050df09f
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2018-12-10 12:15:32 -0500

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyB72ewykSv_BDHYaA2nSD2ZaAGD1QfnDJk",
+    authDomain: "train-scheduler-5d0fd.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5d0fd.firebaseio.com",
+    projectId: "train-scheduler-5d0fd",
+    storageBucket: "train-scheduler-5d0fd.appspot.com",
+    messagingSenderId: "740311317516"
+  };
+  
+  firebase.initializeApp(config);
