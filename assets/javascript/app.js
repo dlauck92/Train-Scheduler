b1ec96bd9e87bba72f55684d7aadaf9d48f1b92e
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2018-12-13 17:46:07 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,11 +37,57 @@ $(document).ready(function(){
             trainFrequency: newFrequency,
         })
 
-    // clear forms after submition - *work in progress*
+    // clear forms after submition
         $("#trainNameInput").val("");
         $("#destinationInput").val("");
         $("#trainTimeInput").val("");
         $("#frequencyInput").val("");
+
+        return false;
+    });
+
+    database.ref().on("child_added", function(snapshot){
+        newRow = $("<tr>");
+        newRow.append("<td>" + snapshot.val().trainName + "</th>");
+        newRow.append("<td>" + snapshot.val().trainDestination + "</th>");
+        newRow.append("<td>" + snapshot.val().trainFrequency + "</th>");
+
+// calculations
+        var start = snapshot.val().trainTime;
+        var frequency = snapshot.val().trainFrequency;
+        var now = moment().format("HH:mm")
+        var next = "";
+
+        var timeDifference = moment().diff(moment(start, "HH:mm"), "minutes")
+
+        if  (timeDifference > 0){
+            next = moment(start, "HH:mm").format("hh:mm a")
+            minutesUntil = moment(start, "HH:mm").diff(moment(), "minutes")
+        }
+
+        else {
+            minutesSince = timeDifference % frequency;
+            minutesUntil = frequency - minutesSince
+            next = moment().add(minutesUntil, "m").format("h:mm a")
+        }
+
+        if (minutesUntil >= 60) {
+            a = minutesUntil
+            var hours = Math.trunc(a/60);
+            var minutes = a % 60;
+            console.log(hours + ":" + minutes)
+            minutesUntil = hours + " hr " + minutes + " min"
+        }
+
+        else {
+            minutesUntil = minutesUntil + " min"
+        }
+
+        newRow.append("<td>" + next + "</td>");
+        newRow.append("<td>" + minutesUntil + "</td>")
+
+        $("#trainTable").append(newRow)
+
     })
 
 
